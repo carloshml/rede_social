d80886467790b6e8122de53eca3ac7576e1965d1
Carlos <hen9@ymail.com>
2025-06-25 11:08:40 -0300

    novo arquivo para melhorar organização

---
+++ views/procurar_pessoas.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+  $('#btn_procurar_pessoa').click(function () {
+    if ($('#nome_pessoa').val().length > 0) {
+      $.ajax({
+        url: '../controllers/usuario-listar-usuarios.php',
+        method: 'post',
+        data: $('#form_procurar_pessoas').serialize(),
+        success: function (data) {
+
+          $('#id_pessoas').html(data);
+
+          $('.btn_seguir').click(function () {
+            var id_usuario = $(this).data('id_usuario');
+            //ocultar botoes mostrar botão correto
+            $('#btn_seguir_' + id_usuario).hide();
+            $('#btn_deixar_seguir_' + id_usuario).show();
+            $.ajax({
+              url: '../controllers/usuario-seguir.php',
+              method: 'post',
+              data: { seguir_id_usuario: id_usuario },
+              success: function (data) {
+                atualizaNumeroSeguidores();
+              }
+            });
+          });
+
+          $('.btn_deixar_seguir').click(function () {
+            var id_usuario = $(this).data('id_usuario');
+            //ocultar botoes mostrar botão correto
+            $('#btn_seguir_' + id_usuario).show();
+            $('#btn_deixar_seguir_' + id_usuario).hide();
+            $.ajax({
+              url: '../controllers/usuario-deixar-seguir.php',
+              method: 'post',
+              data: { deixar_seguir_id_usuario: id_usuario },
+              success: function (data) {
+                atualizaNumeroSeguidores();
+              }
+            });
+          });
+
+        }
+      });
+    }
+  });
+  function atualizaNumeroSeguidores() {
+    $.ajax({
+      url: '../controllers/get_numero_seguidores.php',
+      success: function (data) {
+        $('#numero_seguidores').html(data);
+      }
+    });
+  }
+
+  function atualizaNumeroTwitter() {
+    $.ajax({
+      url: '../controllers/get_numero_tweets.php',
+      success: function (data) {
+        $('#numero_tweets').html(data);
+      }
+    });
+  }
+  atualizaNumeroTwitter();
+  atualizaNumeroSeguidores();
+});
--- views/procurar_pessoas.php
+++ views/procurar_pessoas.php
@@ -18,73 +18,7 @@ if (!isset($_SESSION['usuario'])) {
   <!-- bootstrap - link cdn -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-  <script type="text/javascript">
-    $(document).ready(function () {
-      $('#btn_procurar_pessoa').click(function () {
-        if ($('#nome_pessoa').val().length > 0) {
-          $.ajax({
-            url: '../controllers/usuario-listar-usuarios.php',
-            method: 'post',
-            data: $('#form_procurar_pessoas').serialize(),
-            success: function (data) {
-
-              $('#id_pessoas').html(data);
-
-              $('.btn_seguir').click(function () {
-                var id_usuario = $(this).data('id_usuario');
-                //ocultar botoes mostrar botão correto
-                $('#btn_seguir_' + id_usuario).hide();
-                $('#btn_deixar_seguir_' + id_usuario).show();
-                $.ajax({
-                  url: '../controllers/usuario-seguir.php',
-                  method: 'post',
-                  data: { seguir_id_usuario: id_usuario },
-                  success: function (data) {
-                    atualizaNumeroSeguidores();
-                  }
-                });
-              });
-
-              $('.btn_deixar_seguir').click(function () {
-                var id_usuario = $(this).data('id_usuario');
-                //ocultar botoes mostrar botão correto
-                $('#btn_seguir_' + id_usuario).show();
-                $('#btn_deixar_seguir_' + id_usuario).hide();
-                $.ajax({
-                  url: '../controllers/usuario-deixar-seguir.php',
-                  method: 'post',
-                  data: { deixar_seguir_id_usuario: id_usuario },
-                  success: function (data) {
-                    atualizaNumeroSeguidores();
-                  }
-                });
-              });
-
-            }
-          });
-        }
-      });
-      function atualizaNumeroSeguidores() {
-        $.ajax({
-          url: '../controllers/get_numero_seguidores.php',
-          success: function (data) {
-            $('#numero_seguidores').html(data);
-          }
-        });
-      }
-
-      function atualizaNumeroTwitter() {
-        $.ajax({
-          url: '../controllers/get_numero_tweets.php',
-          success: function (data) {
-            $('#numero_tweets').html(data);
-          }
-        });
-      }
-      atualizaNumeroTwitter();
-      atualizaNumeroSeguidores();
-    });
-  </script>
+  <script language="JavaScript" src="procurar_pessoas.js"></script>
   <link rel="stylesheet" href="../imagens/style.css">
 </head>
 
