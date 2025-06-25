efe40ec857c4f2aa1c1f3c1aeb7898399912989b
Carlos <hen9@ymail.com>
2025-06-25 11:28:19 -0300

    corrigido para atualizar numero de tweets apos deletar

--- views/home.js
+++ views/home.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
         success: function (data) {
           escreverMensagemNaTela('Apagado!');
           atualizaTweet();
+          atualizaNumeroTwitter();
         }
       });
     });
